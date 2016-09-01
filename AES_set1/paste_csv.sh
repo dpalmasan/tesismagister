55a5edb63d7ac176fe7020ec78fb558651c0a7c4
Diego Palma <dpalmasan@gmail.com>
2016-09-01 14:53:58 -0300

    arreglado script para juntar features

--- AES_set1/paste_csv.sh
+++ AES_set1/paste_csv.sh
@@ -1,2 +1,15 @@
-paste -d ',' essay_features.csv adv_giraud.csv d_estimates.csv content.csv > features.csv
-paste -d ',' test_essay_features.csv test_adv_giraud.csv test_d_estimates.csv test_content.csv > test_features.csv
+
+if [ ! -f "essay_features.csv" ] ; then
+  python extraer_features.py
+fi
+
+if [ ! -f "adv_giraud.csv" ] ; then
+  python features_advanced_giraud.py
+fi
+
+if [ ! -f "d_estimates.csv" ] ; then
+  python D_estimate.py
+fi
+
+paste -d ',' essay_features.csv adv_giraud.csv d_estimates.csv > features.csv
+paste -d ',' test_essay_features.csv test_adv_giraud.csv test_d_estimates.csv > test_features.csv
