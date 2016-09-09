2afa6a5b6dda0995f22c6676efe9275527018b2c
Diego Palma <dpalmasan@gmail.com>
2016-09-09 18:47:26 -0300

    content vector analysis

--- AES_set1/content_vector_analysis_attali.R
+++ AES_set1/content_vector_analysis_attali.R
@@ -1,4 +1,4 @@
-library(lsa)
+library(lsa); library(LSAfun)
 load("grades")
 
 #############################################################################
@@ -11,16 +11,51 @@ save(corpus_essays, file = "corpus.RData")
 #############################################################################
 load("corpus_LSA.RData")
 
-aparicion <- corpus_essays != 0
+# Cantidad total de ensayos
+NTOTALESSAYS <- length(grades)
+N <- NTOTALESSAYS - 1
+
+# Propositos de testeo (Se supone que se saca un ensayo y se computan sus caracteristicas)
+id <- 1
+
+# Toma el corpus sin el ensayo al que se le calcularan las caracteristicas
+corpus <- corpus_essays[, -id]
+feat_grades <- grades[-id]
+essay <- corpus_essays[, id]
+  
+# Se sacan los tipos de nota (recordar que hay un solo dato con nota 3)
+grade_types <- unique(feat_grades)
+
+# Indexar en lista conjuntos de ensayos con diferente nota
+l <- list()
+k <- 1
+for (grade in grade_types)
+{
+  l[[k]] <- which(feat_grades == grade)
+  k <- k + 1
+}
+
+# Procesar corpus, eliminar palabras que aparezcan una sola vez (ruido)
+aparicion <- corpus != 0
 rowTotals <- apply(aparicion, 1, sum)
-corpus_essays <- corpus_essays[rowTotals > 1, ]
+corpus.proc <- corpus[rowTotals > 1, ]
 
-# Total number of essays in score 1
-N <- dim(corpus_essays)[2]
+# Vector que tiene la cantidad de veces que ocurre una palabra en todos los ensayos
+Ni <- apply(corpus.proc > 0, 1, sum)
 
-# Cantidad de ensayos en los que aparece la palabra i
-Ni <- rowSums(corpus_essays > 0)
+# Inicializar matriz de pesos
+W <- matrix(0, nrow(corpus.proc), length(grade_types))
 
-test_essays <- textmatrix("test_essays_cleaned", stemming = FALSE,
-                          minWordLength = 3, 
-                          vocabulary = rownames(corpus_essays))
+# Para cada score point category
+for (s in 1:length(grade_types)) {
+  score_category <- corpus.proc[, l[[s]]]
+  if (length(l[[s]]) > 1)
+    Fis <- apply(score_category, 1, sum)
+  else
+    Fis <- score_category
+  W[, s] <- Fis/max(Fis) * log(N/Ni)
+}
+
+# Dejar essay con el vocabulario del corpus
+essay <- essay[rownames(corpus.proc)]
+Wessay <- essay/max(essay) * log(N/Ni)
